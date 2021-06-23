119fa042f96ad5f91620ddbd1418257f655caefb
schollsebastian <scholl.sebastian@gmx.at>
2021-06-23 16:38:44 +0200

    Implement mouse swipe detection

---
+++ swipe-gestures.js
@@ -0,0 +1,50 @@
+const swipeThreshold = window.screen.width * 0.25;
+let clientX;
+let ctrlPressed = false;
+
+window.addEventListener('keydown', e => {
+  if (e.key === 'Control') {
+    ctrlPressed = true;
+  }
+});
+
+window.addEventListener('keyup', e => {
+  if (e.key === 'Control') {
+    ctrlPressed = false;
+  }
+});
+
+window.addEventListener('mousedown', e => {
+  if (ctrlPressed) {
+    swipeStart(e);
+  }
+});
+window.addEventListener('mouseup', e => {
+  if (ctrlPressed) {
+    swipeEnd(e);
+  }
+});
+
+
+function swipeStart(e) {
+  e.preventDefault();
+  clientX = e.clientX;
+}
+
+function swipeEnd(e) {
+  if (clientX) {
+    if (e.clientX - clientX > swipeThreshold) {
+      swipeRight();
+    } else if (clientX - e.clientX > swipeThreshold) {
+      swipeLeft();
+    }
+  }
+}
+
+function swipeRight() {
+  // TODO
+}
+
+function swipeLeft() {
+  // TODO
+}
